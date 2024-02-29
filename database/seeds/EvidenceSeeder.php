6c8c97b89de26784bdb5f49253015d056f378276
daruiza <daruiza@gmail.com>
2024-02-29 10:02:00 -0500

    evidence-storage file set

--- database/seeds/EvidenceSeeder.php
+++ database/seeds/EvidenceSeeder.php
@@ -13,54 +13,55 @@ class EvidenceSeeder extends Seeder
      */
     public function run()
     {
-        // DB::table('evidences')->insert(array(
-        //     'name' => 'evidence 1',
-        //     'file' => 'file 1',
-        //     'type' => 'type 1',
-        //     'description' => 'description 1',
-        //     'approved' => true,
-        //     'focus' => false,
-        //     'task_id' => '3',
-        // ));
+        DB::table('evidences')->insert(array(
+            'name' => 'evidence 1',
+            'file' => '/storage/images/obra001.jpg',
+            'type' => 'type 1',
+            'description' => 'description 1',
+            'approved' => true,
+            'focus' => false,
+            'task_id' => '3',
+        ));
 
-        // DB::table('evidences')->insert(array(
-        //     'name' => 'evidence 2',
-        //     'file' => 'file 2',
-        //     'type' => 'type 2',
-        //     'description' => 'description 2',
-        //     'approved' => true,
-        //     'focus' => false,
-        //     'task_id' => '2',
-        // ));
+        DB::table('evidences')->insert(array(
+            'name' => 'evidnece 2',
+            'file' => '/storage/images/obra002.jpeg',
+            'type' => 'type 2',
+            'description' => 'description 2',
+            'approved' => true,
+            'focus' => false,
+            'task_id' => '2',
+        ));
 
-        // DB::table('evidences')->insert(array(
-        //     'name' => 'evidence 3',
-        //     'file' => 'file 3',
-        //     'type' => 'type 3',
-        //     'description' => 'description 3',
-        //     'approved' => true,
-        //     'focus' => false,
-        //     'task_id' => '1',
-        // ));
+        DB::table('evidences')->insert(array(
+            'name' => 'evidnece 3',
+            'file' => '/storage/images/obra003.jpg',
+            'type' => 'type 3',
+            'description' => 'description 3',
+            'approved' => true,
+            'focus' => false,
+            'task_id' => '1',
+        ));
 
-        // DB::table('evidences')->insert(array(
-        //     'name' => 'evidence 4',
-        //     'file' => 'file 4',
-        //     'type' => 'type 4',
-        //     'description' => 'description 4',
-        //     'approved' => true,
-        //     'focus' => false,
-        //     'task_id' => '2',
-        // ));
+        DB::table('evidences')->insert(array(
+            'name' => 'evidence 4',
+            'file' => '/storage/images/obra004.jpg',
+            'type' => 'type 4',
+            'description' => 'description 4',
+            'approved' => true,
+            'focus' => false,
+            'task_id' => '2',
+        ));
 
-        // DB::table('evidences')->insert(array(
-        //     'name' => 'evidence 5',
-        //     'file' => 'file 5',
-        //     'type' => 'type 5',
-        //     'description' => 'description 5',
-        //     'approved' => true,
-        //     'focus' => false,
-        //     'task_id' => '1',
-        // ));
+        DB::table('evidences')->insert(array(
+            'name' => 'evidence 5',
+            'file' => '/storage/images/obra005.jpg',
+            'type' => 'type 5',
+            'description' => 'description 5',
+            'approved' => true,
+            'focus' => false,
+            'task_id' => '1',
+        ));
+
     }
 }
